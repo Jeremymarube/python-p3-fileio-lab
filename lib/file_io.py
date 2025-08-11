f3dc5a27123d566fada8c876768ae95e1e62b871
Jeremy Marube <jeremymarube1@gmail.com>
2025-08-11 07:48:47 +0300

    Complete file IO lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    # Add .txt extension and write content
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    # Add .txt extension and append content
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    # Add .txt extension and read content
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
